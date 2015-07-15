f061ad598a4360c04bb48bc33a3f0af9e599881c
colin-h <colinharman@utexas.edu>
2015-07-14 23:24:06 -0500

    removed local server settings

--- server/server.js
+++ server/server.js
@@ -206,10 +206,10 @@ app.post('/voteHard', function (req, res){
 
 
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+// var server = app.listen(3000, function () {
+//   var host = server.address().address;
+//   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+//   console.log('Example app listening at http://%s:%s', host, port);
+// });
 
